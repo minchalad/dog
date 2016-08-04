95b43c3f1578cbe5913dd72b90e2758ae39ab2af
Diana Minchala <minchala.diana652@gmail.com>
2016-08-04 09:43:10 -0400

    change1

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(640,640)
+  background(0);
+  }
+
+function draw() {
+background(0);
+fill (200,0,0);
+  ellipse (mouseX,mouseY, 80, 70);
+fill (255, 255, 255);
+  ellipse (mouseX, mouseY, 40, 35);
+  fill(100, 100, 100);
+  ellipse (mouseX, mouseY, 20, 15);
+  }
